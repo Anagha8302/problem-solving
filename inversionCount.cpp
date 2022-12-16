18b1a2a85e40298ff4dc972556fc45fa449d1930
Anagha Rajput <111584300+Anagha8302@users.noreply.github.com>
2022-12-16 22:12:53 +0530

    Add files via upload

---
+++ inversionCount.cpp
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+
+int getInvCount(int arr[], int n)
+{
+    int inv_count = 0;
+    for (int i = 0; i < n - 1; i++)
+        for (int j = i + 1; j < n; j++)
+            if (arr[i] > arr[j])
+                inv_count++;
+
+    return inv_count;
+}
+
+// Driver Code
+int main()
+{
+    int arr[] = { 1, 20, 6, 4, 5 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << " Number of inversions are "
+         << getInvCount(arr, n);
+    return 0;
+}
